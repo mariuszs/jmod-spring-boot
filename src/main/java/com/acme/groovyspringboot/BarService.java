a0aae519f6668c38f861550e9bd0692560acb067
Mariusz Smykula <mariuszs@gmail.com>
2018-09-17 22:08:34 +0200

    cleanup

--- src/main/java/com/acme/groovyspringboot/BarService.java
+++ src/main/java/com/acme/groovyspringboot/BarService.java
@@ -1,12 +1,15 @@
 package com.acme.groovyspringboot;
 
+import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class BarService {
 
 	public void bar() {
-
+		log.info("bar...");
 	}
 
 }
